78fe495cd862137835fb547a5a4c793a710d6a3e
yusufshihata <yusufshihata2006@gmail.com>
2024-11-20 18:25:11 +0000

    Almost there

--- main.py
+++ main.py
@@ -2,39 +2,50 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from fake_useragent import UserAgent
 import random
 import string
+
+
+ua = UserAgent()
+options = webdriver.FirefoxOptions()
+options.add_argument(f'user-agent={ua.random}')
+driver = webdriver.Firefox(options=options)
 
 first_name = "marchal"
 last_name = "mothers"
 password = "jkdjfasdfasfdsf"
 
 
-driver = webdriver.Firefox()
+driver.get("https://accounts.google.com/signup")
 
-def main():
-    driver.get("https://accounts.google.com/signup")
+wait = WebDriverWait(driver, 10)
 
-    wait = WebDriverWait(driver, 80)
-
+def addUsername():
     try:
-# Wait for the page to load and locate form fields
         wait.until(EC.presence_of_element_located((By.ID, "firstName"))).send_keys(first_name)
         driver.find_element(By.ID, "lastName").send_keys(last_name)
 
-# Submit the form (you may need to handle additional steps like CAPTCHA manually)
         driver.find_element(By.XPATH, "//span[text()='Next']").click()
+    except:
+        addUsername()
+
+def setBirth():
+    try:
 
         wait.until(EC.presence_of_element_located((By.XPATH, "//input[@id='day']")))
 
-# Select birthdate (day, month, year)
         driver.find_element(By.XPATH, "//input[@id='day']").send_keys("15")  # Day selection (example)
         driver.find_element(By.XPATH, "//select[@id='month']").send_keys("January")  # Month selection (example)
         driver.find_element(By.XPATH, "//input[@id='year']").send_keys("1990")
         driver.find_element(By.XPATH, "//select[@id='gender']").send_keys("Male")
 
         driver.find_element(By.XPATH, "//span[text()='Next']").click()
+    except:
+        setBirth()
 
+def setEmail():
+    try:
         wait.until(EC.presence_of_element_located((By.XPATH, "//div[@id='selectionc2']")))
 
         button = driver.find_element(By.XPATH, "//div[@class='SCWude']")
@@ -45,19 +56,36 @@ def main():
 
         driver.find_element(By.XPATH, "//span[text()='Next']").click()
 
-    
-        print(f"First name: {first_name}")
-        print(f"Last name: {last_name}")
-        print(f"Email: {gmail}")
-        print(f"password: {password}")
 
     except:
-        main()
+        setEmail()
 
-    passwords = driver.find_elements(By.XPATH, "//input[@class='whsOnd zHQkBf']")
+    return gmail
 
-    for i in passwords:
-        i.send_keys(password)
+def setPassword():
+    try:
+        wait.until(EC.presence_of_element_located((By.XPATH, "//span[@class='VfPpkd-vQzf8d']")))
+        passwordForms = driver.find_elements(By.XPATH, "//input[@class='whsOnd zHQkBf']")
+
+        for passwordForm in passwordForms:
+            passwordForm.send_keys(password)
+
+        driver.find_element(By.XPATH, "//span[text()='Next']").click()
+
+    except:
+        setPassword()
+
+def main():
+
+    addUsername()
+    setBirth()
+    email = setEmail()
+    setPassword()
+
+    print(f"First name: {first_name}")
+    print(f"Last name: {last_name}")
+    print(f"Email: {email}")
+    print(f"password: {password}")
 
 main()
 
